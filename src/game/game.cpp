16b83453f2d82a6a3780879e2ef207c25fd1efc7
William Keske <141369137+williamkeske@users.noreply.github.com>
2025-08-12 21:35:56 -0300

    Ajuste13

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -6237,7 +6237,7 @@ void Game::playerChangeOutfit(uint32_t playerId, Outfit_t outfit, bool setMount,
 		internalCreatureChangeOutfit(player, outfit);
 	}
 	
-	player->internalSetOutfit(outfit);
+	player->setOutfit(outfit);
 
 	auto &playerAttachedEffects = player->attachedEffects();
 	// Wings
